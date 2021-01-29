2ed0ba8e12d284cd43a5eed267e29bd4f696e496
jasoncmc <60374214+jasoncmc@users.noreply.github.com>
2021-01-29 16:45:39 -0500

    Updated
    New Sheep Sprites
    Wolfs
    Some code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0) // if there are any touches on screen
+        {
+            Touch touch = Input.GetTouch(0); //Get the first touch input
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);//converts the touch input into world space coords
+
+            touchPosition.z = 0.0f;
+            transform.position = touchPosition; 
+        }
+    }
+   
+}
