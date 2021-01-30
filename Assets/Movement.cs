b59840c2fd449f17740565dd50797e11edca51a2
kompelchrisk4585 <71229458+kompelchrisk4585@users.noreply.github.com>
2021-01-30 12:21:51 -0500

    r
    gat

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movespeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // movespeed = Random.Range(0, 10);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + movespeed, transform.position.y, -1);
+        
+        //need to destroy when sheep reach a certain range 
+
+
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public float spawnTime;
+    public bool spawnFlag = false;
+    public float spawnDelay; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        
+    }
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (spawnFlag)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
