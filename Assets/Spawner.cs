4072c2d2c01064a686bf70eb9f06bf64dcb123ab
kompelchrisk4585 <71229458+kompelchrisk4585@users.noreply.github.com>
2021-01-31 12:37:22 -0500

    INFINITE SHEEP
    lots of sheep spawner s

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -6,31 +6,55 @@ public class Spawner : MonoBehaviour
 {
     public GameObject spawnee;
     public GameObject spawnee2;
+    public GameObject spawnee3;
+    public GameObject spawnee4;
+    public GameObject spawnee5;
+
     public float spawnTime;
     public bool spawnFlag = false;
     public bool spawnAlt = false;
-    public float spawnDelay;
+    public int spawnDelay;
+    public int randgen; 
     // Start is called before the first frame update
     void Start()
     {
+        spawnDelay = Random.Range(15, 25);
         InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
 
     }
     public void SpawnObject()
     {
-        if (spawnAlt)
+        randgen = Random.Range(1, 6);
+        if (randgen == 1)
         {
             var bumper = (GameObject)Instantiate(spawnee2, transform.position, transform.rotation);
             spawnAlt = false;
         }
-        else
+        else if(randgen == 2)
         {
             var bumper = (GameObject)Instantiate(spawnee, transform.position, transform.rotation);
             spawnAlt = true;
         }
+        else if (randgen == 3)
+        {
+            var bumper = (GameObject)Instantiate(spawnee3, transform.position, transform.rotation);
+            spawnAlt = true;
+        }
+        else if (randgen == 4)
+        {
+            var bumper = (GameObject)Instantiate(spawnee4, transform.position, transform.rotation);
+            spawnAlt = true;
+        }
+        else if (randgen == 5)
+        {
+            var bumper = (GameObject)Instantiate(spawnee5, transform.position, transform.rotation);
+            spawnAlt = true;
+        }
+
         if (spawnFlag)
         {
             CancelInvoke("SpawnObject");
         }
+       
     }
 }
